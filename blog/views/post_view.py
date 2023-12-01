392c15ac53eeb224d6f6cac005e8b97dc4f67d5f
CJosueAK47 <83555240+CJosueAK47@users.noreply.github.com>
2023-12-01 16:24:10 -0300

    Remover submódulo e seta branca

--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
 from django.views import generic
 
-class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello World')
-     
+from blog.models import Post
+
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = "index.html"
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    
